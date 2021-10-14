8d8cafa0b6924e9c4d2554dea0241eeef0ef8aa8
oywl20021020 <3023828897@qq.com>
2021-10-14 15:04:40 +0800

    版本冲突解决

--- OrderDao.java
+++ OrderDao.java
@@ -1,6 +1,12 @@
 ipublic class OrderDao{
   public Integer addOrder(Integer a,Integer b){
+<<<<<<< HEAD
+	Integer c =a+a+b;
+	return c;
+=======
+	//Integer c = a+b;
 	  Integer c =a+a+b;
 	  return c;
+>>>>>>> 6beb217c2d336e76d9ddac451b7d82bab4b21e3c
   }
 }
